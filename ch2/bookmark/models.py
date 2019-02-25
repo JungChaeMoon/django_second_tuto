6092598b7609aa5e669c6c6b3ccfc3bf91d46ecc
ChaeMoon <jungchaemoon@gmail.com>
2019-02-25 18:51:58 +0900

    update model class

--- ch2/bookmark/models.py
+++ ch2/bookmark/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100 , blank=True, null=True)
+    url = models.URLField('url', unique=True)
+
+    def __str__(self):
+        return self.title
+
