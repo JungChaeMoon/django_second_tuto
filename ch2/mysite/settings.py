34e5774bb6bcbd6a8ff5d73be65f78b367f6e700
ChaeMoon <jungchaemoon@gmail.com>
2019-02-25 18:20:53 +0900

    create app

--- ch2/mysite/settings.py
+++ ch2/mysite/settings.py
@@ -103,7 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
