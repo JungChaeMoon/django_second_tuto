5f3c79d097ab2a0bb5b9c5d2695d72e370393e36
ChaeMoon <jungchaemoon@gmail.com>
2019-02-26 19:17:03 +0900

    update url mapping

---
+++ ch2/blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from .views import *
+
+
+urlpatterns=[
+    url(r'^$', PostLV.as_view(), name='index'),
+    url(r'^post/$', PostLV.as_view(), name='post_list'),
+    url(r'^post/(?P<slug>[-\w]+)/$', name='post+_detail'),
+    url(r'^archive/$', PostAV.as_view(), name='post_archive'),
+    url(r'^(?P<year>\d{4})/$', PostYAV.as_view(), name='post_year_archive'),
+    url(r'^()?P<year>\d{4}/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_month_archive'),
+    url(r'^(?P<yeat>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
+    url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
+]
