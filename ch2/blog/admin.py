a9ba2e1167d1f32e6979fd280f303423efd48a5f
ChaeMoon <jungchaemoon@gmail.com>
2019-02-26 17:10:28 +0900

    update admin page

--- ch2/blog/admin.py
+++ ch2/blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from blog.models import Post
+from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date')
+    list_filter = ('modify_date',)
+    search_fields =  ('title', 'content')
+    prepopulated_fields = {'slug':('title',)}
+
+    
+admin.site.register(Post, PostAdmin)
