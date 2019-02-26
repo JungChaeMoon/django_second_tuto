52109f6fc4cd74cf964f4da7aadd4204b53f760f
ChaeMoon <jungchaemoon@gmail.com>
2019-02-26 17:43:54 +0900

    update model class

--- ch2/blog/models.py
+++ ch2/blog/models.py
@@ -2,5 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+from django.core.urlresolvers import reverse
+
 class Post(models.Model):
-    pass
+    title = models.CharField('TITLE', max_length=50, null= True)
+    slug = models.SlugField('SLUG', max_length=50, unique=True, null=True, allow_unicode=True, help_text="one word for title alias.")
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text="simple text")
+    content = models.TextField('CONTENT', null=True)
+    create_date = models.DateField('Create_date', auto_now=True,)
+    modify_date = models.DateField('Modify_date', auto_now=True)
+
+    class  Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'my_post'
+        ordering = ('-modify_date',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
