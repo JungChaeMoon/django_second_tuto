12e132723dd019ceda7cff7da4e49c45526946d3
ChaeMoon <jungchaemoon@gmail.com>
2019-02-27 22:04:47 +0900

    update home view and base template

--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from .views import HomeView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
