69e562bc9fba23b0bc4a9d501d9d5f5a091f52dd
ChaeMoon <jungchaemoon@gmail.com>
2019-03-11 21:26:46 +0900

    update login view

--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 from .views import HomeView
 from .views import UserCreateView, UserCreateDoneTV
 
